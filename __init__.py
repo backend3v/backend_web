bbc08a12ef088c300422f3455e9fcb3ff229d02b
claiunocode <claiunocode@gmail.com>
2024-11-05 13:23:39 -0500

    base

---
+++ __init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from flask import render_template
+import os,json
+app = Flask(__name__)
+
+_port = os.environ.get('PORT', 5000)
+
+@app.route('/')
+def index():
+    data = {"a":"1234"}
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=_port)
