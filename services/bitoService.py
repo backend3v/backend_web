faab3837de967909593e499450e305a247659c21
claiunocode <claiunocode@gmail.com>
2024-11-19 00:26:56 -0500

    version 1.0.0

--- services/bitoService.py
+++ services/bitoService.py
@@ -4,12 +4,24 @@ from deep_translator import GoogleTranslator
 class BitoService:
     def __init__(self,outputLang='en'):
         self.outputLang = outputLang
+        self.languajes= {
+            "es":"Spanish",
+            "en":"English",
+            "fr":"French",
+            "de":"German",
+            "it":"Italian",
+            "pt":"Portuguese",
+            "ru":"Russian",
+            "zh":"Chinese",
+            "ja":"Japanese",
+            "ko":"Korean"
+        }
     def getConfigList(self):
         result = subprocess.run([ 'bito','config','-e'], capture_output=True, text=True, check=True)
         return result
     def getPrompt(self,prompt_text):
         return f"""
-        noticias:Traeme las 3 noticias mas importantes actualmente sobre {prompt_text} e incluye los enlaces de las noticias
+        noticias:Traeme las 3 noticias mas importantes actualmente sobre {prompt_text} tienen que ser noticias relevantes y no mas antiguas de 1 semana 
         comparativa:hazme un csv con campos que se puedan usar para generar un grafico comparativo, busca valores comparables puede ser entre tiempo,region,items afines o contrarios, algun contexto sobre las noticias generadas o otros factores que puedan ser relevantes; sobre el tema de {prompt_text}
         imagen:Traeme una imagen acerca de {prompt_text}, si se trata de una organizacion, empresa, institucion o algo que sea representado con un logo, escudo, simbolo o algo similar; si no se trata de ese caso, puedes darme el enlace de una imagen que represente o parezca a {prompt_text} o algun tema o topico mas general o padre de este tema que si se pueda representar, si no encuentras o no puedes darme el enlace de una imagen, este valor sera nulo
         Generame un archivo json con la siguiente informacion:
@@ -20,7 +32,7 @@ class BitoService:
         variableComparativa:el nombre de la variable que se va a usar para generar el grafico comparativo
         imagen:url de la imagen generada en la pregunta imagen
         noticias:arreglo clave valor json con la siguiente estructura por cada item generado en la pregunta noticias: path:enlace de la noticia,valor:texto de la noticia
-        generame la salida solo con el json, sin ningun tipo de contexto o informacion adicional
+        generame la salida solo con el json, sin ningun tipo de contexto o informacion adicional y la salida debe estar traducida al idioma {self.languajes[self.outputLang]}
         """
     def setConsult(self,prompt_text):
         prompt_text = self.getPrompt(prompt_text)
@@ -29,7 +41,7 @@ class BitoService:
         output = subprocess.check_output(('bito'), stdin=ps.stdout)
         ps.wait()
         output = str(output.decode('UTF-8'))
-        output = self.translate(output,self.outputLang)
+        #output = self.translate(output,self.outputLang)
         output = output.replace('```json','').replace('```','')
         output = json.loads(output)
         return output
