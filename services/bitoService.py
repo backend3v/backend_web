343bac7f4ab664620bcb0ccbacf27dd1ebeb8b60
claiunocode <claiunocode@gmail.com>
2024-11-19 15:04:51 -0500

    version 1.0.1

--- services/bitoService.py
+++ services/bitoService.py
@@ -23,7 +23,7 @@ class BitoService:
         return f"""
         noticias:Traeme las 3 noticias mas importantes actualmente sobre {prompt_text} tienen que ser noticias relevantes y no mas antiguas de 1 semana de preferencia noticias que hayan sido polemicas, importantes y relevantes para {prompt_text}, deben ser descriptivas y con un estilo periodistico, deben tener los detalles y no dejar temas inconclusos, deben ser cortas pero completas, traer nombres propios, lugares, y muy importante deben tener la fecha de su publicacion y la fuente en la descripcion de esta noticia, buscalas de fuentes oficiales y evita en lo posible fake news y fuentes de dudosa procedencia
         comparativa:hazme un csv con campos que se puedan usar para generar un grafico comparativo, busca valores comparables puede ser entre tiempo,region,items afines o contrarios, algun contexto sobre las noticias generadas o otros factores que puedan ser relevantes; sobre el tema de {prompt_text}
-        imagen:Traeme una imagen acerca de {prompt_text}, si se trata de una organizacion, empresa, institucion o algo que sea representado con un logo, escudo, simbolo o algo similar; si no se trata de ese caso, puedes darme el enlace de una imagen que represente o parezca a {prompt_text} o algun tema o topico mas general o padre de este tema que si se pueda representar, si no encuentras o no puedes darme el enlace de una imagen, este valor sera nulo
+        imagen:Traeme una imagen acerca de {prompt_text}, si se trata de una organizacion, empresa, institucion o algo que sea representado con un logo, escudo, simbolo o algo similar; si no se trata de ese caso, puedes darme el enlace de una imagen que represente o parezca a {prompt_text} o algun tema o topico mas general o padre de este tema que si se pueda representar,los enlaces deben ser de sitios oficiales, wikipedia o alguno de los sitios de las fuentes en donde encontraste las noticias de la pregunta noticias, si no encuentras o no puedes darme el enlace de una imagen, este valor sera nulo
         descripcionComparativa: Traeme una descripcion acerca de la comparativa, que contenga temas relevantes o sucesos que describan las variaciones en los valores comparables de la pregunta comparativa
         Generame un archivo json con la siguiente informacion:
         topic:{prompt_text}
@@ -35,6 +35,7 @@ class BitoService:
         news:arreglo clave valor json con la siguiente estructura por cada item generado en la pregunta noticias: path:enlace de la fuente donde encontraste el item de la noticia,valor:texto completo de la noticia generada en la pregunta noticias
         comparative_description:texto de la descripcion generada en la pregunta descripcionComparativa
         colors:un arreglo con 3 colores que tengan referencia al tema de {prompt_text}, que sean brillantes o contrasten bien con un fondo oscuro
+        phrase:una frase representativa, segundo nombre con el que se conoce,slogan, subtitulo, nickname ,apodo o algun dicho o frase popular que represente a {prompt_text}
         generame la salida solo con el json, sin ningun tipo de contexto o informacion adicional y la salida debe estar traducida al idioma {self.languajes[self.outputLang]},excepto las claves del json deben estar en ingles tal cual esta en la pregunta y en minusculas
         """
     def setConsult(self,prompt_text):
